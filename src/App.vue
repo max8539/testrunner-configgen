c48561f6b24f1b0445af9830178ae4e1b10ec7d4
max8539 <maxyuen8539@gmail.com>
2024-12-06 02:46:37 +1100

    Add license, remove editorconfig

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
-import DisplayStorage from './components/DisplayStorage.vue';
+  import DisplayStorage from './components/DisplayStorage.vue';
 </script>
 
 <template>
   <div class="p-2 sm:p-4">
-    <div class="w-full 2xl:w-[1280px] mx-auto p-4 bg-white dark:bg-black rounded-lg sm:rounded-2xl drop-shadow-lg sm:drop-shadow-2xl text-slate-800 dark:text-slate-200">
+    <div class="w-full 2xl:w-[1280px] mx-auto p-4 bg-white dark:bg-black rounded-lg sm:rounded-2xl drop-shadow-lg sm:drop-shadow-2xl font-sans text-slate-800 dark:text-slate-200">
       <RouterView />
       <DisplayStorage />
     </div>
